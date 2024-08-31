d38bf64ceefea84a17e5736ace354bd1e411c23b
Soumyadip Maity <22053029@kiit.ac.in>
2024-08-31 22:01:32 +0530

    chore: Update App.js to use react-router-dom for routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="">
-      <h1 className='bg-black text-red-500'>Hello</h1>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
