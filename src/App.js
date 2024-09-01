226c644b6ac8c4f808392f52604ce85c4d9910be
Soumyadip Maity <22053029@kiit.ac.in>
2024-09-02 00:14:14 +0530

    feat: Add Navbar and Footer components to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        
       </Routes>
+      <Footer/>
     </Router>
   );
 }
