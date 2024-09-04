ecf8e7af9140e255259043578485fb4bbd6aeab8
Soumyadip Maity <22053029@kiit.ac.in>
2024-09-04 18:23:38 +0530

    feat: Update App.js to include Blog page in the routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Navbar from './components/navbar';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        
+        <Route path="/blog" element={<Blog />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
